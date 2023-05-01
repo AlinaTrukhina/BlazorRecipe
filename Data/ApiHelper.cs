46ed901c1389544fbbfd48f8bb026eca95ab055e
AlinaTrukhina <alina.trukhina@gmail.com>
2023-05-01 10:24:54 -0500

    init commit

---
+++ Data/ApiHelper.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BlazorRecipe.Data
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
